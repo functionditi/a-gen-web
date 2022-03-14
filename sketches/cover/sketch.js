efb36bc4b93c7bf037241c13cd92f703552eb7f7
functionditi <aduneti.9626@gmail.com>
2022-03-14 01:20:46 -0700

    edits

--- sketches/cover/sketch.js
+++ sketches/cover/sketch.js
@@ -1,12 +1,17 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(300);
+  
+  
 }
 
 function draw() {
- fill(255);
+ noFill();
+ stroke(0, 255, 0);
+ strokeWeight(3);
 
-  stroke(0, 255, 0);
+fill(255);
+  
   strokeWeight(3);
   ellipse(mouseX, mouseY, 25);
 }
